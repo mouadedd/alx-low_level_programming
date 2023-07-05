0c7303a5be12d9d09c5c3784f990e460eb634e73
mouadedd <mdx.edd@gmail.com>
2023-07-05 03:03:07 -0700

    betty style checked

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y
+ * _pow_recursion - value of x raised to the power of y
  * @x: value
  * @y: power
  * Return: result of x raised to the power of y
  */
+
 int _pow_recursion(int x, int y)
 {
 	if (y == 0)
