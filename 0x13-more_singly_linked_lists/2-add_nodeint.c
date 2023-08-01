06aeab04d7cb96448d06f9342d3beb9a36675e00
mouadedd <mdx.edd@gmail.com>
2023-08-01 17:11:36 +0000

    quick fix

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (!new)
 		return (NULL);
 	(*new).n = n;
-	(*New).next = *head;
+	(*new).next = *head;
 	*head = new;
-	return (new);
+		return (new);
 }
