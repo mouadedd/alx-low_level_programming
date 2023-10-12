ced317363d8b895fed3e6643cf8956b53e213933
mouadedd <mdx.edd@gmail.com>
2023-10-12 03:58:58 -0700

    fixed variables

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	dlistint_t *new_node;
 	dlistint_t *traverse;
-	unsigned int positon;
+	unsigned int position;
 
 	if (h == NULL)
 		return (NULL);
