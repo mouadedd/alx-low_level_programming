266ef87a0dd0c937d44ee3d60cea5403e101bb30
mouadedd <mdx.edd@gmail.com>
2023-10-12 03:57:37 -0700

    add node at a position

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+/**
+ *insert_dnodeint_at_index- inserts node at a position.
+ *@h: pointer to ptr to list
+ *@idx: index of the node
+ *@n: the integer to the new node
+ *Return: address of the new node, or NULL
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+
+	dlistint_t *new_node;
+	dlistint_t *traverse;
+	unsigned int positon;
+
+	if (h == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	(*new_node).n = n;
+	if (idx == 0)
+	{
+		(*new_node).next = *h;
+		(*new_node).prev = NULL;
+		if ((*new_node).next != NULL)
+			new_node->next->prev = new_node;
+		*h = new_node;
+	}
+	else
+	{
+		traverse = *h;
+		for (position = 0; traverse != NULL && position < (idx - 1); position++)
+			traverse = (*traverse).next;
+		if (traverse == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		(*new_node).next = (*traverse).next;
+		(*new_node).prev = traverse;
+		if ((*traverse).next != NULL)
+			traverse->next->prev = new_node;
+		(*traverse).next = new_node;
+	}
+	return (new_node);
+}
