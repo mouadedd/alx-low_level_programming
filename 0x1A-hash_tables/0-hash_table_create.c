e973ee56c6fc76190472faa53bd336c2a5056663
mouadedd <mdx.edd@gmail.com>
2023-11-03 03:29:27 -0700

    task 1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ *hash_table_create- creator of a hash table
+ *@size : table's size
+ *Return: table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *p;
+
+	p = malloc(sizeof(hash_table_t));
+
+	if (p == NULL)
+		return (NULL);
+
+	(*p).size = size;
+	(*p).array = malloc(sizeof(hash_node_t *) * size);
+
+	if ((*p).array == NULL)
+	{
+		free((*p).array);
+		return (NULL);
+	}
+	return (p);
+}
